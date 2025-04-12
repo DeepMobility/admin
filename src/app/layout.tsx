bed66254239933f63f682f54e0beb1b10a86110d
forster <groux.forster@gmail.com>
2025-04-12 16:56:31 +0200

    remove dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Viewport, Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -13,9 +13,14 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "DeepMobility admin",
+  title: "Deepmobility Admin",
+  description: "Admin de Deepmobility",
   robots: { index: false }
 };
+
+export const viewport: Viewport = {
+  themeColor: 'only light',
+}
 
 export default function RootLayout({
   children,
