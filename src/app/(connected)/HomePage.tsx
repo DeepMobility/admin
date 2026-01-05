c67a7e820e4f10f0a22514f5f13dc928ffaeb487
Jamaïca Servier <jamaica.svr@gmail.com>
2026-01-05 19:17:53 +0100

    update accounts list view

--- src/app/(connected)/HomePage.tsx
+++ src/app/(connected)/HomePage.tsx
@@ -65,7 +65,7 @@ export default function HomePage({ accounts }: { accounts: Array<any> }) {
             </div>
             <div className="w-56">
               <a href={getDashboardUrl(account.host)} target="_blank">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-6 h-6 text-blue-500"><path d="M13.5 10.5L21 3m-5 0h5v5m0 6v5a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-black mx-auto"><path d="M13.5 10.5L21 3m-5 0h5v5m0 6v5a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5"/></svg>
               </a>
             </div>
             <button type="button" onClick={() => deleteAccount(account.id)}
